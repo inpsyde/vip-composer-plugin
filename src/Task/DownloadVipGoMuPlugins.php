ecf4421e68d0a0a39fd2d96777aa94b3a6850d04
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2020-09-22 18:08:16 +0200

    Fix DownloadVipGoMuPlugins::enabled()

--- src/Task/DownloadVipGoMuPlugins.php
+++ src/Task/DownloadVipGoMuPlugins.php
@@ -63,7 +63,7 @@ final class DownloadVipGoMuPlugins implements Task
     public function enabled(TaskConfig $taskConfig): bool
     {
         return ($taskConfig->isLocal() || $taskConfig->forceVipMuPlugins())
-            && !$taskConfig->skipCoreUpdate();
+            && !$taskConfig->skipVipMuPlugins();
     }
 
     /**
